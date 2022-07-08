30786dbe6b5c450f1a9e30514f5fc5df1f3cff48
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-07-08 23:54:17 +0700

    Perbaikan Tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: DetailScreen(),
     );
   }
 }
